d3fa3384f1cb6ccf828c2cdcb53e22b51c1ea2bc
Chris Morris <cmorris@ccistudios.com>
2017-03-16 16:47:08 -0400

    style the updated reports archive section

--- sites/all/themes/lawss/template.php
+++ sites/all/themes/lawss/template.php
@@ -8,21 +8,3 @@ function lawss_form_alter(&$form, &$form_state, $form_id )
 		$form['search_block_form']['#attributes']['placeholder'] = t('Search');
 	}
 }
-
-function lawss_views_pre_render($view) {
-  if ($view->name == 'reports') {
-
-    if ($view->current_display == 'page') {
-
-    	if(empty($view->result))
-    	{
-	      $view->set_title($view->args[0]);
-    	}
-      }
-    }
-
-	  if ($view->current_display == 'page_1')
-	  {
-	  		$view->set_title($view->args[0]." Archive");
-	  }
-  }
